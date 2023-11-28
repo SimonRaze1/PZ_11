4d31a9fa9627b04d90e9e1cce2eed6c96a8721fa
shirokov.dd2226 <shirokov.dd2226@COM409--R-5>
2023-11-28 14:41:30 +0500

    Добавьте файлы проекта.

---
+++ PZ_12/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace PZ_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 5;
+            double b = 45;
+            double c = 125;
+            double d = 245;
+            double AMeanAB, GMeanAB;
+            double AMeanAC, GMeanAC;
+            double AMeanAD, GMeanAD;
+            Mean(a, b, out AMeanAB, out GMeanAB);
+            Mean(a, c, out AMeanAC, out GMeanAC);
+            Mean(a, d, out AMeanAD, out GMeanAD);
+            Console.WriteLine("Средние значения: Арифметическое - " + AMeanAB + ", и геометрическое - " + GMeanAB);
+            Console.WriteLine("Средние значения: Арифметическое - " + AMeanAC + ", и геометрическое - " + GMeanAC);
+            Console.WriteLine("Средние значения: Арифметическое - " + AMeanAD + ", и геометрическое - " + GMeanAD);
+        }
+        static void Mean(double x, double y, out double AMean, out double GMean)
+            {
+                AMean = (x + y) / 2;
+                GMean = Math.Sqrt(x * y);
+            }
+    }
+}
